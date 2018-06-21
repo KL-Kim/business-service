bcffec0301dd16b5f5d6f9ba792706458f569469
KL-Kim <jinguanglong11@hotmail.com>
2018-06-21 14:35:01 +0800

    June 21st

--- tests/init.business.js
+++ tests/init.business.js
@@ -1,49 +1,56 @@
 const db = require('../config/db.config');
 import Business from '../models/business.model';
+import Category from '../models/category.model';
 
-  let business, i;
+let business, i;
 
-  let yidaba = new Business({
-    cnName: "一大把",
-    krName: "이따바",
-    enName: "yidaba",
-    tel: '123-1234-1234',
-    category: '5b28b6df8e9e57f92bb8c5ef',
-    status: 'PUBLISHED',
-  });
-
-  yidaba.save((err, item) => {
-    if (err) throw err;
-
-    console.log(item.enName, ' saved');
-  });
-
-  let xilaiguan = new Business({
-    cnName: "喜来馆",
-    krName: "희래관",
-    enName: "xilaiguan",
-    tel: '123-1234-1234',
-    category: '5b28b6df8e9e57f92bb8c5f2',
-    status: 'PUBLISHED',
-  });
-
-  xilaiguan.save((err, item) => {
-    if (err) throw err;
-
-    console.log(item.enName, ' saved');
-  });
-
-  for(i = 1; i <= 100; i++) {
-    business = new Business({
-      cnName: i,
-      krName: i,
-      enName: i,
-      category: '5b28b6df8e9e57f92bb8c5ee',
-      tel: i,
+Category.findOne({ code: 11 }).exec()
+  .then(category => {
+    let yidaba = new Business({
+      cnName: "一大把",
+      krName: "이따바",
+      enName: "yidaba",
+      tel: '123-1234-1234',
+      category: category._id.toString(),
       status: 'PUBLISHED',
     });
 
-    business.save().then(item => {
-      console.log(item.cnName + " saved");
+    yidaba.save((err, item) => {
+      if (err) throw err;
+
+      console.log(item.enName, ' saved');
     });
-  }
+
+    let xilaiguan = new Business({
+      cnName: "喜来馆",
+      krName: "희래관",
+      enName: "xilaiguan",
+      tel: '123-1234-1234',
+      category: category._id.toString(),
+      status: 'PUBLISHED',
+    });
+
+    xilaiguan.save((err, item) => {
+      if (err) throw err;
+
+      console.log(item.enName, ' saved');
+    });
+
+    for(i = 1; i <= 100; i++) {
+      business = new Business({
+        cnName: i,
+        krName: i,
+        enName: i,
+        category: category._id.toString(),
+        tel: i,
+        status: 'PUBLISHED',
+      });
+
+      business.save().then(item => {
+        console.log(item.cnName + " saved");
+      });
+    }
+  })
+  .catch(err => {
+    throw err;
+  })
